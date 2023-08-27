6e61a700259671f1717211d7165bf35a9d2db013
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-27 14:39:16 -0600

    Save instantiating BeatBox with @list = init_list method, before testing different ways to instantiate BeatBox with a list

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -12,21 +12,21 @@ class BeatBox
   end
   
   def init_list(data)
-    if data
-      if @list.head
-        self.list.append(data)
-      else
-        @lisst = LinkedList.new.append(data) : LinkedList.new
-      end
-      
+    if data && self.list
+      self.list.append(data)
+    elsif data && !self.list
+      @list = LinkedList.new.append(data)
+    else
+      @list = LinkedList.new
+    end
   end
 
   def get_valid_beats(data)
     data_arr = data.split(" ")
     valid_beats = []
-    if data_arr.length > 1
+    if data_arr.length > 0
       valid_beats = data_arr.select{ |beat|
-        @all.include?(beat) || beat.length < 5
+        @all.include?(beat) || beat.length <= 5
       }
     end
     valid_beats.each{ |beat|
@@ -38,17 +38,21 @@ class BeatBox
   end
 
   def append(data)
-    if get_valid_beats(data)
-      data.split(" ").each{ |beat|
-        self.list.append(beat)
+    valid_arr = get_valid_beats(data)
+    if valid_arr.length > 0
+      valid_arr.split(" ").each{ |beat|
+        @list.append(beat)
+        add_to_all(beat)
       }
     end
   end
 
   def prepend(data)
-    if get_valid_beats(data)
+    valid_arr = get_valid_beats(data)
+    if valid_arr.length > 0
       data.split(" ").each{ |beat|
-        self.list.prepend(beat)
+        @list.prepend(beat)
+        add_to_all(beat)
       }
     end
   end
@@ -61,11 +65,16 @@ class BeatBox
     beats = self.list.to_string
     `say -r 350 -v Boing #{beats}`
   end
+
+  def add_to_all(data)
+    @all << data if @all.include?(data)
+  end
 end
 
 bb = BeatBox.new("deep")
 bb.append('Mississippi womp woop bebop dowop')
 p bb.list
+p bb.all
 # bb.prepend('tee tee tee deep')
 # bb.append('deep doo ditt woo hoo shu')
 # binding.pry
