b89239e3350873c6d13b2592169ed4cdb0676e2e
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-26 23:53:18 -0600

    Add traverse_list helper method to LinkedList class

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,36 +1,24 @@
 require './lib/node'
 require 'dead_end'
-# require 'pry'
 
 class LinkedList
   attr_reader :head
   def initialize
     @head = nil
   end
+  def get_node(pos)
+    node = @head
+    pos.times{
+      node = node.next_node
+    }
+    node
+  end
   def append(data)
     if !@head
       @head = Node.new(data)
     else
-      append_to_tail(@head, data)
+      get_node(count-1).next_node = Node.new(data)
     end
-    def append_to_tail(node, data)
-      if !node.next_node
-        node.next_node = Node.new(data)
-      else
-        append_to_tail(node.next_node, data)
-      end
-    end
-    data
-  end
-
-  def count(node=@head, count=0)
-    return count if !node
-    count(node.next_node, count += 1)
-  end
-
-  def to_string(node=@head)
-    return node.data if !node.next_node
-    "#{node.data} #{to_string(node.next_node)}"
   end
 
   def prepend(data)
@@ -45,28 +33,30 @@ class LinkedList
 
   def insert(idx, data, node=@head)
     if idx > count || idx < 0
-      puts "Sorry, value not in range."
+      puts "Sorry, #{idx} value not in range of #{count} nodes"
       return
     end
     new_node = Node.new(data)
-    if idx == 0
-      new_node.next_node = @head
-      @head = new_node
-    elsif idx == 1
-      new_node.next_node = node.next_node
-      node.next_node = new_node
-    else
-      insert(idx-1, data, node.next_node)
-    end
+    node_before = get_node(idx-1)
+    node_behind = node_before.next_node
+    node_before.next_node = new_node
+    new_node.next_node = node_behind
+  end
+
+  def count(node=@head, count=0)
+    return count if !node
+    count(node.next_node, count += 1)
+  end
+
+  def to_string(node=@head)
+    return node.data if !node.next_node
+    "#{node.data} #{to_string(node.next_node)}"
   end
 
   def find(idx,len)
     return 'Sorry, that is not a valid range.' if (idx + len) > count
-    start_node = head
+    start_node = get_node(idx)
     node_str = ""
-    idx.times {
-      start_node = start_node.next_node
-    }
     len.times {
       node_str = node_str + " " + start_node.data
       start_node = start_node.next_node
@@ -88,15 +78,30 @@ class LinkedList
   end
 
   def pop
-    def penult_node
-      node = @head
-      (count-2).times {
-        node = node.next_node
-      }
-      popped = node.next_node
-      node.next_node = nil
-      popped.data
-    end
-    penult_node
+    new_tail = get_node(count-2)
+    popped = new_tail.next_node
+    new_tail.next_node = nil
+    popped
   end
 end
+
+list = LinkedList.new
+list.append('la')
+list.append('dee')
+list.append('da')
+list.append('doh')
+list.append('we')
+list.append('likes')
+list.append('to')
+list.append('party')
+p list.count
+p list.traverse_list(3).data
+list.insert(3,'ho')
+p list.count
+list.insert(6,'hum')
+p list.count
+p list.to_string
+p list.pop.data
+p list.count
+p list.to_string
+p list.find(0,8)
