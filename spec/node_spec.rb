e2215f8cbff28d2787b6ac0462a98a57acc049fd
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-27 21:08:25 -0600

    Test instantiation, full functionality of Node class

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,14 +2,14 @@ require './lib/node'
 
 describe Node do
   it 'exists' do
-    node = Node.new('plop')
+    node = Node.new
     expect(node).to be_a Node
   end
-  it 'stores data' do
+  it 'holds a single element of data' do
     node = Node.new('plop')
     expect(node.data).to eq('plop')
   end
-  it 'can link to another node' do
+  it 'holds a link to another node' do
     node = Node.new('plop')
     expect(node.next_node).to eq(nil)
 
@@ -18,6 +18,7 @@ describe Node do
 
     node_1.next_node = node_2
 
+    expect(node_1.data).to eq('bang')
     expect(node_1.next_node).to be_a Node
     expect(node_1.next_node.data).to eq('boom')
   end
